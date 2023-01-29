497734fbfb37952dff033aa38f4aa4fb1740f777
Angela Dai <zhjh.angela@gmail.com>
2023-01-29 13:46:54 -0500

    add files

---
+++ Assets/Scripts/AR/UI/CloseOnOutsideClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CloseOnOutsideClick : MonoBehaviour
+{
+    public GameObject infoUI;
+    public GameObject title;
+
+    private void Start()
+    {
+        infoUI = transform.parent.gameObject;
+    }
+    void Update()
+    {
+        if ((Input.touchCount > 0 || Input.GetMouseButtonDown(0)) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            title.SetActive(false);
+            //panel.SetActive(false);
+            infoUI.SetActive(false);
+        }
+    }
+
+}
